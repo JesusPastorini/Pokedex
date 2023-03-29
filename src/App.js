1b24b3bcc1f37714312fcb0a0993581be1ce28c9
JesusPastorini <juniorpastorini_@hotmail.com>
2023-03-29 18:34:15 -0300

    iniciando

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Pokemon from './Pokemon';
 
 class App extends React.Component {
   render() {
     return (
+      <div>
       <h1>Exercise - Pokedex</h1>
+      <Pokemon />
+      </div>
     );
   }
 }
